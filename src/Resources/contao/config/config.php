b8e031edb7be3d70c1e82b91d5b4719cfa41be3b
Mathias Arzberger <arzberger@pdir.de>
2022-11-22 10:06:26 +0100

    fix warning in console

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -10,23 +10,25 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
-$GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE'] = array_merge_recursive(
-    (array) $GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE'],
-    [
-        'contao' => [
-            'member_token_login' => [
-                'recipients' => ['recipient_email'],
-                'email_subject' => ['domain', 'link', 'member_*', 'recipient_email'],
-                'email_text' => ['domain', 'link', 'member_*', 'recipient_email'],
-                'email_html' => ['domain', 'link', 'member_*', 'recipient_email', 'login_form_html'],
-                'file_name' => ['domain', 'link', 'member_*', 'recipient_email'],
-                'file_content' => ['domain', 'link', 'member_*', 'recipient_email'],
-                'email_sender_name' => ['recipient_email'],
-                'email_sender_address' => ['recipient_email'],
-                'email_recipient_cc' => ['recipient_email'],
-                'email_recipient_bcc' => ['recipient_email'],
-                'email_replyTo' => ['recipient_email'],
+if(isset($GLOBALS['NOTIFICATION_CENTER'])) {
+    $GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE'] = array_merge_recursive(
+        (array)$GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE'],
+        [
+            'contao' => [
+                'member_token_login' => [
+                    'recipients' => ['recipient_email'],
+                    'email_subject' => ['domain', 'link', 'member_*', 'recipient_email'],
+                    'email_text' => ['domain', 'link', 'member_*', 'recipient_email'],
+                    'email_html' => ['domain', 'link', 'member_*', 'recipient_email', 'login_form_html'],
+                    'file_name' => ['domain', 'link', 'member_*', 'recipient_email'],
+                    'file_content' => ['domain', 'link', 'member_*', 'recipient_email'],
+                    'email_sender_name' => ['recipient_email'],
+                    'email_sender_address' => ['recipient_email'],
+                    'email_recipient_cc' => ['recipient_email'],
+                    'email_recipient_bcc' => ['recipient_email'],
+                    'email_replyTo' => ['recipient_email'],
+                ],
             ],
-        ],
-    ]
-);
+        ]
+    );
+}
